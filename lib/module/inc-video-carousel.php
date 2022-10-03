d35cc09b02ff6440819d8084296f816adad64cb4
TFMteam <114900437+TFMteam@users.noreply.github.com>
2022-10-03 09:04:49 +0300

    Add files via upload

---
+++ lib/module/inc-video-carousel.php
@@ -0,0 +1,88 @@
+<div class="container fill">
+	<div id="myCarousel" class="carousel slide">
+		<div class="carousel-inner">
+			<!-- Carousel Slide -->
+			<div class="item active">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<div class="video-btn-play">
+								</div>
+								<div class="video-ico-service">
+								</div>
+								<div class='video-con-animate page-video-1'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/videos/1.jpg') no-repeat center; background-size: cover;">
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-4">
+								<div class='video-emp-block'>
+								</div>
+								<h2>КОНЕЦ СВЯЗИ(Трейлер)</h2>
+								<h3>Всем привет! Chelik & Xomak на связи, это трейлер к нашему сериалу  "Конец связи", мы первый раз делаем подобного рода видео поэтому просим простить за мелкие косяки и т.д. Также сейчас мы зарабатываем на нашу мечту при этом работая над проектом и учась в школе, поэтому кто хочет нас поддержать пожертвуйте денежек по ссылочке(поддерживающих будем показывать в конце видео):
+https://www.donationalerts.com/r/team...</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Carousel Slide -->
+			<div class="item">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<div class="video-btn-play">
+								</div>
+								<div class="video-ico-service">
+								</div>
+								<div class='video-con-animate page-video-2'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/videos/2.jpg') no-repeat center; background-size: cover;">
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-5">
+								<div class='video-emp-block'>
+								</div>
+								<h2>ГОСПОДИ ПОМИЛУЙ!(Phasmophobia)</h2>
+								<h3>Первое видео по Phasmophobia</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Carousel Slide -->
+			<div class="item">
+				<div class="fill">
+					<div class="video-con-container">
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<div class="video-btn-play">
+								</div>
+								<div class="video-ico-service">
+								</div>
+								<div class='video-con-animate page-video-3'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/videos/3.jpg') no-repeat center; background-size: cover;">
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-6">
+								<div class='video-emp-block'>
+								</div>
+								<h2>Что скрывает от меня эта игрушка? Tattletail (СДЕЛАЙТЕ ПОТИШЕ)</h2>
+								<h3>Я разделил полное видео на 2 части, эта первая, монтажа здесь нет, поскольку не вижу смысла вставлять мемы и т.д в хоррор.</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
